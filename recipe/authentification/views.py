6a94a7978b4b280cc34e6759bfd362aa03c9b80f
mariam <BLmariam1611@gmail.com>
2023-05-05 20:19:06 +0100

    add logout

--- recipe/authentification/views.py
+++ recipe/authentification/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # # Create your views here.
 
 def signup(request):
@@ -9,7 +10,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             # Rediriger vers une page de confirmation ou de connexion
-            return redirect('/')
+            return redirect('signIn')
     else:
         form = CustomUserCreationForm()
     return render(request, 'signUp.html', {'form': form})
@@ -20,10 +21,16 @@ def login_view(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return render(request, 'signIn.html')
+            login(request, user) 
+            return redirect('home')
         else:
             error_message = 'Invalid username or password'
     else:
         error_message = ''
     return render(request, 'signIn.html', {'error_message': error_message})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request,("You have been logout"))
+    return redirect('home')
